c142ad987efef01a385e6406007745168508c8b2
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-03-12 11:11:04 -0500

    Update Ejercicio5Javascript.js
    Se añade la función flecha

--- Ejercicio5Javascript.js
+++ Ejercicio5Javascript.js
@@ -1,23 +1,5 @@
 // Ejercicio 5, sables de luz negativos y positivos. 
 
-// Aplicando método forEach. 
-
-//const sablesdeLuzTotales = [1, -4, 2, -2, 5, -7, 8, -1];
-//
-//const sablesPositivos = [];
-//const sablesNegativos = [];
-//
-//sablesdeLuzTotales.forEach(num => {
-//  if (num >= 0) {
-//    sablesPositivos.push(num);
-//  } else {
-//    sablesNegativos.push(num);
-//  }
-//});
-//
-//console.log("La cantidad de sables con energía positiva es: ", sablesPositivos);
-//console.log("La cantidad de sables con energía negativa es: ", sablesNegativos);
-
 // Aplicando función tradicional con operador ternario.
 
 const sablesdeLuzTotales = [1, -4, 2, -2, 5, -7, 8, -1];
@@ -35,3 +17,16 @@ function calcularSables(){
 }
 
 console.log(calcularSables())
+
+// En funcion flecha y operador ternario
+
+// const calcularSables = () => {
+// 	sablesdeLuzTotales.forEach(num => {
+// 		num >= 0 ? 
+// 		sablesPositivos.push(num) :
+// 		sablesNegativos.push(num); 
+// })
+// 	return "La cantidad de sables contada fue: " + sablesdeLuzTotales + " Los sables positivos fueron: " + sablesPositivos + " y los sables negativos fueron: " + sablesNegativos;
+// }
+
+// console.log(calcularSables())
